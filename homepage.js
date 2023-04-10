c96cdd40e1fea59b1178c8973b9b2df3a8ccd36b
Allen <arusman1987@gmail.com>
2023-04-10 15:17:02 +0800

    fdfdfa

---
+++ homepage.js
@@ -0,0 +1,102 @@
+var image = document.getElementById("kulas");
+  var paragraph = document.querySelector(".moro");
+
+  image.addEventListener("mouseover", function() {
+    paragraph.style.display = "block";
+  });
+
+  image.addEventListener("mouseout", function() {
+    paragraph.style.display = "none";
+  });
+
+  var image2 = document.getElementById("kulas2");
+  var paragraph2 = document.querySelector(".moro2");
+
+  image2.addEventListener("mouseover", function() {
+    paragraph2.style.display = "block";
+  });
+
+  image2.addEventListener("mouseout", function() {
+    paragraph2.style.display = "none";
+  });
+  var image3 = document.getElementById("kulas3");
+  var paragraph3 = document.querySelector(".moro3");
+
+  image3.addEventListener("mouseover", function() {
+    paragraph3.style.display = "block";
+  });
+
+  image3.addEventListener("mouseout", function() {
+    paragraph3.style.display = "none";
+  });
+  var image4 = document.getElementById("kulas4");
+  var paragraph4 = document.querySelector(".moro4");
+
+  image4.addEventListener("mouseover", function() {
+    paragraph4.style.display = "block";
+  });
+
+  image4.addEventListener("mouseout", function() {
+    paragraph4.style.display = "none";
+  });
+  var image5 = document.getElementById("kulas5");
+  var paragraph5 = document.querySelector(".moro5");
+
+  image5.addEventListener("mouseover", function() {
+    paragraph5.style.display = "block";
+  });
+
+  image5.addEventListener("mouseout", function() {
+    paragraph5.style.display = "none";
+  });
+  var image6 = document.getElementById("kulas6");
+  var paragraph6 = document.querySelector(".moro6");
+
+  image6.addEventListener("mouseover", function() {
+    paragraph6.style.display = "block";
+  });
+
+  image6.addEventListener("mouseout", function() {
+    paragraph6.style.display = "none";
+  });
+
+  var image7 = document.getElementById("kulas7");
+  var paragraph7 = document.querySelector(".moro7");
+
+  image7.addEventListener("mouseover", function() {
+    paragraph7.style.display = "block";
+  });
+
+  image7.addEventListener("mouseout", function() {
+    paragraph7.style.display = "none";
+  });
+  var image8 = document.getElementById("kulas8");
+  var paragraph8 = document.querySelector(".moro8");
+
+  image8.addEventListener("mouseover", function() {
+    paragraph8.style.display = "block";
+  });
+
+  image8.addEventListener("mouseout", function() {
+    paragraph8.style.display = "none";
+  });
+  var image9 = document.getElementById("kulas9");
+  var paragraph9 = document.querySelector(".moro9");
+
+  image9.addEventListener("mouseover", function() {
+    paragraph9.style.display = "block";
+  });
+
+  image9.addEventListener("mouseout", function() {
+    paragraph9.style.display = "none";
+  });
+  var image10 = document.getElementById("kulas10");
+  var paragraph10 = document.querySelector(".moro10");
+
+  image10.addEventListener("mouseover", function() {
+    paragraph10.style.display = "block";
+  });
+
+  image10.addEventListener("mouseout", function() {
+    paragraph10.style.display = "none";
+  });
